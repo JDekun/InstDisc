3156cd49a889bb6ea75736de03279dab2ba99f8c
JDekun <jurry_k@outlook.com>
2022-07-29 11:20:26 +0800

    local

--- cifar.py
+++ cifar.py
@@ -62,10 +62,10 @@ transform_test = transforms.Compose([
 ])
 
 trainset = datasets.CIFAR10Instance(root='../../input', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=2048, shuffle=True, num_workers=2)
 
 testset = datasets.CIFAR10Instance(root='../../input', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 ndata = trainset.__len__()
--- test.py
+++ test.py
@@ -106,7 +106,7 @@ def kNN(epoch, net, lemniscate, trainloader, testloader, K, sigma, recompute_mem
             dist = torch.mm(features, trainFeatures)
 
             yd, yi = dist.topk(K, dim=1, largest=True, sorted=True)
-            candidates = trainLabels.view(1,-1).expand(batchSize, -1)
+            candidates = trainLabels.contiguous().view(1,-1).expand(batchSize, -1)
             retrieval = torch.gather(candidates, 1, yi)
 
             retrieval_one_hot.resize_(batchSize * K, C).zero_()
